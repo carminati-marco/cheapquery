afd95233753d52c0ff034f53614b21969dc08a7f
“carminati-marco” <marco.c@taboola.com>
2022-12-01 10:37:45 +0100

    integrated partitioning + create utils

--- arrow_v1.py
+++ arrow_v1.py
@@ -3,12 +3,14 @@ from datetime import timedelta
 
 import pandas as pd
 import pyarrow as pa
+import pyarrow.dataset as da
 import pyarrow.parquet as pq
 
 publisher_domain_ids = [1525074, 1525073, 1597725, 1525077]
 publisher_id = 74968
 from_transaction_date = "2022-11-03"
 to_transaction_date = "2022-11-04"
+
 
 class DF_COLUMNS:
     PUBLISHER_ID = "publisher_id"
@@ -25,9 +27,15 @@ def _get_table(file_info):
     :param file_info:
     :return:
     """
+    _partitioning = da.partitioning(schema=pa.schema([pa.field("publisher_id", pa.int64()),
+                                                      pa.field("publisher_domain_id", pa.int64())
+                                                      ]),
+                                    flavor='hive')
+
     print("[INIT] _get_table", file_info.base_name, datetime.datetime.now())
     gcs = pa.fs.GcsFileSystem(anonymous=False, retry_time_limit=timedelta(seconds=15))
     table = pq.read_table(file_info.path,
+                          partitioning=_partitioning,
                           columns=[DF_COLUMNS.PUBLISHER_ID,
                                    DF_COLUMNS.PUBLISHER_DOMAIN_ID,
                                    DF_COLUMNS.ORDER_AMOUNT,
@@ -35,105 +43,43 @@ def _get_table(file_info):
                                    DF_COLUMNS.PUBLISHER_COMMISSION_AMOUNT,
                                    DF_COLUMNS.SALES_COUNT_TOTAL],
                           filters=[(DF_COLUMNS.PUBLISHER_ID, '=', publisher_id),
-                                   (DF_COLUMNS.PUBLISHER_DOMAIN_ID, 'in', publisher_domain_ids)],
-                          filesystem=gcs).to_pandas()
+                                   (DF_COLUMNS.PUBLISHER_DOMAIN_ID, 'in', publisher_domain_ids),
+                                   (DF_COLUMNS.TRANSACTION_DATE, '>=', from_transaction_date),
+                                   (DF_COLUMNS.TRANSACTION_DATE, '<', to_transaction_date)
+                                   ],
+                          filesystem=gcs
+                          ).to_pandas()
     return table
 
 
-def get_df(uri, is_file=True):
+def get_df(uri):
     """
     Obtains the df, reading the uri.
     :param uri:
-    :param is_file: false if the "uri" directory must be processed.
     :return:
     """
     print("[INIT] get_df", datetime.datetime.now())
-    gcs = pa.fs.GcsFileSystem(anonymous=False, retry_time_limit=timedelta(seconds=15))
-
-    file_list = [pa.fs.FileInfo(uri)] if is_file else gcs.get_file_info(pa.fs.FileSelector(uri, recursive=False))
-    frames = []
-    for _file in file_list:
-        frames.append(_get_table(_file))
+    df = _get_table(pa.fs.FileInfo(uri))
     print("[END] get_df", datetime.datetime.now())
-    return pd.concat(frames)
-
-    # TODO: INTEGRATE async in _get_table
-    # event_loop = asyncio.get_event_loop()
-    # asyncio.set_event_loop(event_loop)
-    # futures = []
-    # for _file in file_list:
-    #     futures.append(event_loop.create_task(_get_table(_file, redis_connector)))
-    # d = event_loop.run_until_complete(asyncio.gather(*futures))
-    # event_loop.close()
-    # # print("end get_df", datetime.datetime.now())
-    # return d
-
-    # for f in file_list:
-    #     print("init", datetime.datetime.now())
-    #     df = pd.read_csv(f"gs://{f.path}")
-    #     print(datetime.datetime.now())
-    #     df.to_parquet(f.base_name.replace(".csv", ".parquet"))
-    #     print("end", datetime.datetime.now())
-
-    # print(datetime.datetime.now())
-    # df = pd.read_csv('gs://data-events-test/cheapquery/dataset/90-day-enriched-commissions000000000001.csv')
-    # print(datetime.datetime.now())
-    # df.to_parquet("90-day-enriched-commissions000000000001.parquet")
-    #
-    # gcs = pa.fs.GcsFileSystem(anonymous=False, retry_time_limit=timedelta(seconds=15))
-    #
-    # # List all contents in a bucket, recursively
-    # uri = "data-events-test/cheapquery/dataset/arrow"
-    # file_list = gcs.get_file_info(pa.fs.FileSelector(uri, recursive=True))
-    #
-    # with pa.memory_map('bigfile.arrow', 'rb') as source:
-    #     with pa.ipc.open_file(source) as reader:
-    #         df = reader.read_pandas()
-    #         print(df)
-
-
-def apply_query(df):
-    """
-    Function to filter/group/sup the dataframe df
-    """
-    # TODO: extract the filter as params (to evaluate).
-
-    df = df[(df[DF_COLUMNS.PUBLISHER_ID] == publisher_id) &
-            (df[DF_COLUMNS.PUBLISHER_DOMAIN_ID].isin(publisher_domain_ids)) &
-            (df[DF_COLUMNS.TRANSACTION_DATE] >= from_transaction_date) &
-            (df[DF_COLUMNS.TRANSACTION_DATE] < to_transaction_date)]
-
-    df[DF_COLUMNS.TRANSACTION_DATE] = pd.to_datetime(df[DF_COLUMNS.TRANSACTION_DATE])
-    df = df.groupby([df[DF_COLUMNS.TRANSACTION_DATE].dt.date])[DF_COLUMNS.ORDER_AMOUNT,
-                                                               DF_COLUMNS.PUBLISHER_COMMISSION_AMOUNT,
-                                                               DF_COLUMNS.SALES_COUNT_TOTAL].sum()
     return df
+
+
+def apply_group(df):
+    """
+    Function to group/sum the dataframe df
+    """
+    df[DF_COLUMNS.TRANSACTION_DATE] = pd.to_datetime(df[DF_COLUMNS.TRANSACTION_DATE])
+    return df.groupby([df[DF_COLUMNS.TRANSACTION_DATE].dt.date])[DF_COLUMNS.ORDER_AMOUNT,
+                                                                 DF_COLUMNS.PUBLISHER_COMMISSION_AMOUNT,
+                                                                 DF_COLUMNS.SALES_COUNT_TOTAL].sum()
 
 
 if __name__ == '__main__':
     print("[INIT] main", datetime.datetime.now())
-    df = get_df(uri="data-events-test/cheapquery/parquet", is_file=False)
+    df = get_df(uri="data-events-test/cheapquery/parquet_partitioning")
     # df = get_df(uri="data-events-test/cheapquery/parquet/enriched-comms-74968-60-days.parquet", is_file=True)
     print(df.count())
-    df_1 = apply_query(df)
+    df_1 = apply_group(df)
     df_1.to_csv("result.csv")
-    print(df.count())
+    print(df_1.count())
     print("[END] main", datetime.datetime.now())
-
-
-#### UTILS.
-def convert_files_to_parquet(uri="data-events-test/cheapquery/csv"):
-    """
-    utils. lists a directory in GCS and save the csv file in parquet format.
-    :param uri:
-    """
-    gcs = pa.fs.GcsFileSystem(anonymous=False, retry_time_limit=timedelta(seconds=15))
-    file_list = gcs.get_file_info(pa.fs.FileSelector(uri, recursive=True))
-    print(datetime.datetime.now())
-    for f in file_list:
-        print("[INIT]", datetime.datetime.now())
-        df = pd.read_csv(f"gs://{f.path}")
-        print(datetime.datetime.now())
-        df.to_parquet(f.base_name.replace(".csv", ".parquet"))
-        print("[END]", datetime.datetime.now())
-    print(datetime.datetime.now())
---
+++ utils.py
@@ -0,0 +1,35 @@
+import datetime
+from datetime import timedelta
+
+import pandas as pd
+import pyarrow as pa
+
+from arrow_v1 import DF_COLUMNS
+
+
+def partition_data(df):
+    from dask import dataframe as dd
+    nyc_payroll_ddexport = dd.from_pandas(df, npartitions=1)
+    nyc_payroll_ddexport.to_parquet(
+        path="./partition",
+        engine='pyarrow',
+        compression='snappy',
+        partition_on=[DF_COLUMNS.PUBLISHER_ID, DF_COLUMNS.PUBLISHER_DOMAIN_ID]
+    )
+
+
+def convert_files_to_parquet(uri="data-events-test/cheapquery/csv"):
+    """
+    utils. lists a directory in GCS and save the csv file in parquet format.
+    :param uri:
+    """
+    gcs = pa.fs.GcsFileSystem(anonymous=False, retry_time_limit=timedelta(seconds=15))
+    file_list = gcs.get_file_info(pa.fs.FileSelector(uri, recursive=True))
+    print(datetime.datetime.now())
+    for f in file_list:
+        print("[INIT]", datetime.datetime.now())
+        df = pd.read_csv(f"gs://{f.path}")
+        print(datetime.datetime.now())
+        df.to_parquet(f.base_name.replace(".csv", ".parquet"))
+        print("[END]", datetime.datetime.now())
+    print(datetime.datetime.now())
